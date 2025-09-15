c227c56ea29c934f2d0ffde7d2ef76f2bc092934
Mayank Yadav <mayank24343@iiitd.ac.in>
2025-09-15 15:26:14 +0000

    Created basic code structure

---
+++ fib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fib(int x){
+	if (x == 0){
+		return 0;
+	}
+	else if (x == 1){
+		return 1;
+	}
+	
+	return fib(x-1)+fib(x-2);
+}
+int main(){
+	int x; 
+	scanf("%d",&x);
+	printf("Fibonaaci result for %d is %d",x,fib(x));
+}
--- shell.c
+++ shell.c
@@ -1,2 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int global = 0;
+
+void show_history(){
+	printf("I am displaying the history\n");
+}
+
+int create_process_and_run(char* command){
+	if (strcmp(command,"exit\n")==0){
+		return 0; //exit the shell
+	}
+	else if (strcmp(command,"history\n") == 0){
+		//show history of commands received
+		show_history();
+	}
+
+
+	int status = fork();
+	if (status < 0){
+		printf("Could not create child process!");
+	}
+	else if (status == 0){
+		char *args[64];
+		int i = 0;
+
+		//loop to collect tokens from command separated by space or \n
+		char *token = strtok(command, " \n");
+		while (token != NULL && i < 63) {
+			if (strcmp(token,"|") == 0){
+				//pipes in input, do differently
+				printf("detected pipe in this input.\n");
+			}
+			else if (strcmp(token,"cd") == 0){
+				//cd detected, do differently
+				printf("cd detected\n");
+			}
+
+			args[i++] = token;
+			token = strtok(NULL," \n");
+		}
+
+		args[i] = NULL; // execvp requires NULL-terminated argv
+		execvp(args[0], args);
+
+	}
+
+	wait(NULL);
+	return status;
+} 
+
+int launch(char* command){
+	int status;
+	status = create_process_and_run(command);
+	return status;
+}
+
+void shell_loop(){
+	int status;
+	do {
+		printf("group60@os:~$");
+		char* command = (char*)malloc(1024*sizeof(char)); //1KB input
+		fgets(command,1024,stdin);
+		status = launch(command);
+	} while (status);
+}
+
+int main(){
+	shell_loop();
+	return 0;
+}
 
